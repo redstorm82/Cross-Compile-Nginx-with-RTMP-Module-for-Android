92a8f43bf6c4e4fb8a59f0359b1cc73ca1dccaaa
felix <redstorm82@qq.com>
2019-05-21 15:01:31 +0800

    add start\stop\reload script

--- install_to_stb.sh
+++ install_to_stb.sh
@@ -4,4 +4,10 @@ adb root
 touch sdcard/nginx/logs/fake
 adb push sdcard/nginx /data
 adb push start_nginx.sh /data/nginx/start.sh
-adb shell chmod 777 /data/nginx/start.sh
+adb push stop_nginx.sh /data/nginx/stop.sh
+adb push reload_nginx.sh /data/nginx/reload.sh
+adb push nginx_proxy.conf /data/nginx/conf/nginx.conf
+adb shell chmod 777 /data/nginx/*.sh
+adb shell /data/nginx/stop.sh
+adb shell /data/nginx/start.sh
+adb shell ps | grep nginx
---
+++ reload_nginx.sh
@@ -0,0 +1,1 @@
+/data/nginx/sbin/nginx  -p /data/nginx/  -s reload
